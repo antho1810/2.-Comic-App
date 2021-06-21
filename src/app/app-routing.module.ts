b1d20750492ea92169621addb2e7a7633ab3a157
Anthony Romero <iaromerod@unac.edu.co>
2021-06-21 09:45:03 -0500

    Subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent  },
+  { path: 'heroes', component: HeroesComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'buscar/:termino', component: BuscadorComponent},
+  { path: 'heroe/:id', component: HeroeComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
